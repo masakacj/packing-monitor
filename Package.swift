fcc6cf7a5a89d607670fd698a75a3caf94203db4
masakacj <34531511+masakacj@users.noreply.github.com>
2026-08-28 20:15:44 +0800

    build: link CoreImage for browser preview encoding

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
             ],
             linkerSettings: [
                 .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
                 .linkedFramework("CoreMedia")
             ]
         )
