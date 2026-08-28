38167ec6992d86fce331b8b383c74bef8129ac0d
masakacj <34531511+masakacj@users.noreply.github.com>
2026-08-28 20:06:39 +0800

    build: add Swift service package

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "PackingMonitor",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "PackingMonitor", targets: ["PackingMonitor"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/hummingbird-project/hummingbird.git",
+            from: "2.26.0"
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "PackingMonitor",
+            dependencies: [
+                .product(name: "Hummingbird", package: "hummingbird")
+            ],
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreMedia")
+            ]
+        )
+    ]
+)
