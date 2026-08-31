beefa3bd624762926ff4b0dd2a6a7eebee51eecd
masakacj <34531511+masakacj@users.noreply.github.com>
2026-08-31 16:39:44 +0800

    Support macOS 10.15 and remove modern server dependency

--- Package.swift
+++ Package.swift
@@ -1,31 +1,25 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 5.3
 
 import PackageDescription
 
 let package = Package(
     name: "PackingMonitor",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v10_15)
     ],
     products: [
         .executable(name: "PackingMonitor", targets: ["PackingMonitor"])
     ],
-    dependencies: [
-        .package(
-            url: "https://github.com/hummingbird-project/hummingbird.git",
-            from: "2.26.0"
-        )
-    ],
+    dependencies: [],
     targets: [
-        .executableTarget(
+        .target(
             name: "PackingMonitor",
-            dependencies: [
-                .product(name: "Hummingbird", package: "hummingbird")
-            ],
+            dependencies: [],
             linkerSettings: [
                 .linkedFramework("AVFoundation"),
                 .linkedFramework("CoreImage"),
-                .linkedFramework("CoreMedia")
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("Network")
             ]
         )
     ]
